8983961caab9bcd347cf5eaf9df00ef1e94fd25d
João Vitor <jvitoroliveira0711@gmail.com>
2026-01-11 16:48:03 -0300

    feat: Utiliza funcao run para exibir imagem selecionada

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-use pixels::{Pixels, SurfaceTexture};
-use winit::{
-    dpi::LogicalSize,
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::{Window, WindowBuilder, WindowId},
-};
-
 fn main() {
     let args = match siv::cli::parse() {
         Ok(a) => a,
@@ -15,12 +7,7 @@ fn main() {
         }
     };
 
-    let mut window: Option<Window> = None;
-    let mut window_id: Option<WindowId> = None;
-    let mut pixels: Option<Pixels> = None;
-
-    let path = args.path;
-    let image = match siv::infra::load_image(&path) {
+    let image = match siv::infra::load_image(&args.path) {
         Ok(i) => i,
         Err(msg) => {
             eprint!("{msg}");
@@ -28,78 +15,8 @@ fn main() {
         }
     };
 
-    let width = image.width;
-    let height = image.height;
-    let rgba = image.rgba;
-
-    let event_loop = match EventLoop::new() {
-        Ok(l) => l,
-        Err(err) => {
-            eprintln!("Erro ao criar loop de eventos: {}", err);
-            return;
-        }
-    };
-
-    let _ = event_loop.run(move |event, elwt| {
-        elwt.set_control_flow(ControlFlow::Wait);
-
-        match event {
-            Event::Resumed => {
-                if window.is_none() {
-                    let w = WindowBuilder::new()
-                        .with_title("Simple image viewer")
-                        .with_inner_size(LogicalSize::new(width as f64, height as f64))
-                        .build(elwt)
-                        .unwrap();
-
-                    window_id = Some(w.id());
-                    window = Some(w);
-                }
-
-                let win = window.as_ref().unwrap();
-                if pixels.is_none() {
-                    let size = win.inner_size();
-                    let surface = SurfaceTexture::new(size.width, size.height, win);
-                    let p = Pixels::new(width, height, surface).unwrap();
-                    pixels = Some(p);
-                }
-
-                win.request_redraw();
-            }
-
-            Event::WindowEvent {
-                window_id: eid,
-                event,
-                ..
-            } => {
-                if Some(eid) != window_id {
-                    return;
-                }
-
-                match event {
-                    WindowEvent::CloseRequested => elwt.exit(),
-
-                    WindowEvent::Resized(new_size) => {
-                        if let Some(p) = pixels.as_mut() {
-                            let _ = p.resize_surface(new_size.width, new_size.height);
-                        }
-                        if let Some(w) = window.as_ref() {
-                            w.request_redraw();
-                        }
-                    }
-
-                    WindowEvent::RedrawRequested => {
-                        if let Some(p) = pixels.as_mut() {
-                            p.frame_mut().copy_from_slice(&rgba);
-                            p.render().unwrap();
-                        }
-                    }
-
-                    _ => {}
-                }
-            }
-
-            _ => {}
-        }
-    });
+    if let Err(msg) = siv::app::run(image, &args) {
+        eprint!("{msg}");
+        std::process::exit(2);
+    }
 }
